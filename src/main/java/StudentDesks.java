78fbfbc48b6e3179ab0ccc6172c24c8eb93d4f9c
Luke Graham <luke.2313356@nv.ccsd.net>
2020-09-04 16:30:20 +0000

    Completed SecondsPastMidnight, Timestamps, and StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,19 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int group1 = scanner.nextInt();
+        int group2 = scanner.nextInt();
+        int group3 = scanner.nextInt(); 
 
         /*
          *  your code goes here
          */
+        int group1desk = group1/2 + group1%2;
+        int group2desk = group2/2 + group2%2;
+        int group3desk = group3/2 + group3%2;
 
+        int totaldesk = group1desk + group2desk + group3desk;
+        System.out.println(totaldesk);
 
         // closing the scanner object
         scanner.close();
