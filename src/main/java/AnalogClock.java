c3ea256b772ee79de8490e0aff2a930496afb4cc
Luke Graham <luke.2313356@nv.ccsd.net>
2020-09-04 16:50:53 +0000

    Worked on AnalogClock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,11 +7,15 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-
+        int angleInput = scanner.nextInt();
 
         /*
          *  your code goes here
          */
+         int hour = angleInput/30;
+
+         int angleH = 2*(angleInput%30);
+         System.out.println(angleH);
 
         // closing the scanner object
         scanner.close();
